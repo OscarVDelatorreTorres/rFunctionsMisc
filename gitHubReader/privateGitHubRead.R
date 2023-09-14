a3476bf6a54036e5f47def7ad78719ca98491921
Dr. Oscar V. De la Torre Torres <110574678+OscarVDelatorreTorres@users.noreply.github.com>
2023-09-14 15:20:43 -0600

    Create privateGitHubRead.R

---
+++ gitHubReader/privateGitHubRead.R
@@ -0,0 +1,19 @@
+# Esta librería es para leer de repositorios de github privados como los míos:
+
+library(httr)
+
+privateGitHubRead=function(GitHubURL,github_email,github_personal_access_token){
+script <-
+    GET(
+      url=GitHubURL,
+      authenticate({github_email}, {github_personal_access_token}),     # Instead of PAT, could use password
+      accept("application/vnd.github.v3.raw")
+    ) %>%
+    content(as = "text")
+  
+  # Evaluate and parse to global environment
+  #eval(parse(text = script))
+  source(script)
+
+print("GitHub private repository reading function uploaded in you R environment...")
+}
